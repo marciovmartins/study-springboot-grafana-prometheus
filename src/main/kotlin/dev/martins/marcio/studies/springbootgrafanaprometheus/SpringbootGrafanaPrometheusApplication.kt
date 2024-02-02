1639688f5a08949a202ac082e8bf4e59ce8139ee
Marcio Martins <marciovmartins@hotmail.com>
2024-02-02 13:09:12 +0100

    Implementation of Spring Boot with Prometheus and Grafana.

---
+++ src/main/kotlin/dev/martins/marcio/studies/springbootgrafanaprometheus/SpringbootGrafanaPrometheusApplication.kt
@@ -0,0 +1,11 @@
+package dev.martins.marcio.studies.springbootgrafanaprometheus
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootGrafanaPrometheusApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringbootGrafanaPrometheusApplication>(*args)
+}
---
+++ src/test/kotlin/dev/martins/marcio/studies/springbootgrafanaprometheus/SpringbootGrafanaPrometheusApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.martins.marcio.studies.springbootgrafanaprometheus
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootGrafanaPrometheusApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
